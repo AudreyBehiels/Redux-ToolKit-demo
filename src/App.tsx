e17ca2aac5f598257d3d125b2f03f3f786001f01
Audrey Behiels <audrey.behiels@gmail.com>
2022-08-25 15:06:52 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 import ItemForm from './app/components/ItemForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ItemForm />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ItemForm />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/app/components/ItemForm.tsx
+++ src/app/components/ItemForm.tsx
@@ -1,42 +1,42 @@
 import React, { FC, useState } from 'react';
 
 const ItemForm: FC = () => {
-  const [itemTitle, setItemTitle] = useState<string>('');
-  const [itemDesc, setItemDesc] = useState<string>('');
+    const [itemTitle, setItemTitle] = useState<string>('');
+    const [itemDesc, setItemDesc] = useState<string>('');
 
-  return (
-    <>
-      <h3>Create todo list</h3>
-      <div className='container'>
-        <div>
-          <label className='row'>
-            <h4 className='col-4 text-right'>Title:</h4>
-            <input
-              className='col-8'
-              type='text'
-              value={itemTitle}
-              placeholder='e.g. Clean up room'
-            ></input>
-          </label>
-          <label className='row'>
-            <h4 className='col-4 text-right'>Description:</h4>
-            <textarea
-              id='description'
-              className='col-8'
-              rows={2}
-              cols={20}
-              value={itemDesc}
-              placeholder='bedroom, ...'
-            ></textarea>
-          </label>
-        </div>
-      </div>
+    return (
+        <>
+            <h3>Create todo list</h3>
+            <div className="container">
+                <div>
+                    <label className="row">
+                        <h4 className="col-4 text-right">Title:</h4>
+                        <input
+                            className="col-8"
+                            type="text"
+                            value={itemTitle}
+                            placeholder="e.g. Clean up room"
+                        ></input>
+                    </label>
+                    <label className="row">
+                        <h4 className="col-4 text-right">Description:</h4>
+                        <textarea
+                            id="description"
+                            className="col-8"
+                            rows={2}
+                            cols={20}
+                            value={itemDesc}
+                            placeholder="bedroom, ..."
+                        ></textarea>
+                    </label>
+                </div>
+            </div>
 
-      <button type='button' className='btn btn-primary'>
-        Add
-      </button>
-    </>
-  );
+            <button type="button" className="btn btn-primary">
+                Add
+            </button>
+        </>
+    );
 };
 
 export default ItemForm;
